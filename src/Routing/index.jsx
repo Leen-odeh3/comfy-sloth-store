7dc9fb9e15cca61ba699023e8ff40a71fcc27b9d
leen <leenodeh287@gmail.com>
2023-10-30 21:31:15 +0300

    update routing

--- src/Routing/index.jsx
+++ src/Routing/index.jsx
@@ -1,6 +1,9 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "../Pages/Home";
 import Header from './../Component/Header/Header'
+import Cart from '../Pages/Cart'
+import About from '../Pages/About'
+import Products from '../Pages/Products'
 
 const Routing = () => {
   return (
@@ -9,7 +12,9 @@ const Routing = () => {
       <Routes>
         <Route path="/" element={<Home/>}>
           <Route path="/home" element={<Home />} />
-          
+          <Route path="/cart" element={<Cart/>} />
+          <Route path="/about" element={<About/>} />
+          <Route path="/products" element={ <Products/>} />
         </Route>
       </Routes>
     </BrowserRouter>
