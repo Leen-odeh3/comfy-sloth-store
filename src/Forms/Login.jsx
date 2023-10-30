713bc43018d1d71210f0701d816b39047310bc53
leen <leenodeh287@gmail.com>
2023-10-30 16:34:48 +0300

    update project files

---
+++ src/Forms/Login.jsx
@@ -0,0 +1,11 @@
+import './Style.scss'
+
+const Login = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Login
---
+++ src/Forms/Register.jsx
@@ -0,0 +1,11 @@
+import './Style.scss'
+
+const Register = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Register
