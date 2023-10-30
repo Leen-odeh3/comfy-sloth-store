f5d071b5e61c4c07d9fd5f234934355066532d3f
leen <leenodeh287@gmail.com>
2023-10-30 19:57:12 +0300

    done header but not responsive

--- src/Component/Header/Header.jsx
+++ src/Component/Header/Header.jsx
@@ -1,13 +1,33 @@
-import './Header.scss'
-import TopHeader from './TopHeader'
+import { NavLink } from "react-router-dom";
+import "./Header.scss";
+import TopHeader from "./TopHeader";
 
 const Header = () => {
+  const navs = ["Home", "About", "Products", "Cart"];
+
   return (
     <div>
-      <TopHeader/>
-     
-    </div>
-  )
-}
+      <TopHeader />
 
-export default Header
+      <div className="main-header">
+        <div className="title">
+          <h1>C</h1>
+        </div>
+        <div className="links">
+          {navs.map((e, index) => (
+            <NavLink active key={index} to={`/${e}`}>
+              {e}
+            </NavLink>
+          ))}
+        </div>
+
+        <div className="icons">
+          <i className="bi bi-moon-fill"></i>
+          <i className="bi bi-cart3"></i>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
