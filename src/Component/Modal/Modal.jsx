422d871cbfb1ade7d1ef70839f30a0ac27bb1fec
leen <leenodeh287@gmail.com>
2023-11-08 22:42:28 +0300

    loading products

--- src/Component/Modal/Modal.jsx
+++ src/Component/Modal/Modal.jsx
@@ -1,20 +1,38 @@
 import { useParams } from "react-router-dom"
 import { useEffect, useState } from "react";
-import axios from "axios";
+// import axios from "axios";
 import './Modal.scss'
 
 const Modal = () => {
-  const {id}=useParams();
-  const [product, setProduct] = useState([]);
+  const { id } = useParams();
 
+  const productId = +id;
+  const [product, setProduct] = useState(null);
   useEffect(() => {
-    axios
-      .get(`https://strapi-store-server.onrender.com/api/products/${id}`)
-      .then((res) => setProduct(res.data.data))
-      .catch((error) => console.error(error));
-  });
+    const fetchData = async () => {
+      try {
+        const response = await fetch(`https://strapi-store-server.onrender.com/api/products/${productId}`);
+
+        if (response.ok) {
+          const data = await response.json();
+          setProduct(data);
+        } else {
+          throw new Error(`Failed to fetch product (Status: ${response.status})`);
+        }
+      } catch (error) {
+        console.error(error);
+      } 
+    };
+
+    fetchData();
+  }, [id]);
 
 
+  if (!product || !product.attributes || !product.attributes.data) {
+    return <h1>Loading ...</h1>;
+    
+}
+else{
   return (
     <div className="p-page">
 <img src={product.attributes.image}/>
@@ -26,9 +44,10 @@ const Modal = () => {
 <p>{product.attributes.description}</p>
 <button>Add TO Cart</button>
 </div>
+
     </div>
 
-  )
+  )}
 }
 
 export default Modal
